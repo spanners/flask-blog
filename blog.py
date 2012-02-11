40cd021a16e661291d4900c773c9e929abc4a28b
Simon Buist <hashcollide@gmail.com>
2012-02-11 16:43:40 +0000

    Add pagination

--- blog.py
+++ blog.py
@@ -15,23 +15,18 @@ from flask import url_for
 from flaskext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-# configuration
-DEBUG = True
-SQLALCHEMY_DATABASE_URI='sqlite:///blog.db'
-SECRET_KEY = "development-key"
 
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('BLOG_SETTINGS', silent=True)
-db = SQLAlchemy(app)
+APP = Flask(__name__)
+APP.config.from_envvar('BLOG_SETTINGS', silent=True)
+DB = SQLAlchemy(APP)
 
 
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    text = db.Column(db.Text)
-    pub_date = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class Posts(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    title = DB.Column(DB.String(80))
+    text = DB.Column(DB.Text)
+    pub_date = DB.Column(DB.DateTime)
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'))
 
     def __init__(self, user, title, text, pub_date=None):
         self.user = user
@@ -42,11 +37,11 @@ class Posts(db.Model):
     def __repr__(self):
         return '<Posts %r>' % self.title
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(120))
-    posts = db.relationship(Posts, lazy='dynamic', backref='user')
+class User(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    username = DB.Column(DB.String(80), unique=True)
+    password = DB.Column(DB.String(120))
+    posts = DB.relationship(Posts, lazy='dynamic', backref='user')
 
     def __init__(self, username, password):
         self.username = username
@@ -55,40 +50,65 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-@app.template_filter('datetimeformat')
+
+@APP.template_filter('datetimeformat')
 def datetimeformat(value, format='%H:%M on %d/%m/%Y'):
     return value.strftime(format)
 
-@app.before_request
+
+@APP.before_request
 def before_request():
     g.user = None
     if 'user_id' in session:
         g.user = User.query.get(session['user_id'])
 
-@app.route('/')
-def show_posts():
-    posts = Posts.query.filter_by(user=g.user)
-    return render_template('show_entries.html', posts=posts)
 
-@app.route('/add', methods=['POST'])
+PER_PAGE = 5
+
+@APP.route('/')
+@APP.route('/posts/', defaults={'page': 1})
+@APP.route('/posts/page/<int:page>')
+def show_posts(page=1):
+    posts = get_posts_for_page(page, PER_PAGE)
+    pagination = Posts.query.filter_by(user=g.user).paginate(page, PER_PAGE)
+    return render_template('show_entries.html',
+        pagination=pagination,
+        posts=posts
+    )
+
+def get_posts_for_page(page, per_page):
+    all_posts = Posts.query.filter_by(user=g.user)
+    start = (page * per_page) - per_page
+    stop = start + per_page
+    return all_posts.slice(start, stop)
+
+def url_for_other_page(page):
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+
+APP.jinja_env.globals['url_for_other_page'] = url_for_other_page
+
+
+@APP.route('/add', methods=['POST'])
 def add_post():
     if not session.get('logged_in'):
         abort(401)
     post = Posts(g.user, request.form['title'], request.form['text'])
-    db.session.add(post)
-    db.session.commit()
+    DB.session.add(post)
+    DB.session.commit()
     flash('New post was successfully posted')
     return redirect(url_for('show_posts'))
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@APP.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         user = User.query.filter_by(username=request.form['username']).first()
         if user and user.password == request.form['password']:
-            db.session.add(user)
-            db.session.commit()
+            DB.session.add(user)
+            DB.session.commit()
             session['user_id'] = user.id
             session['logged_in'] = True
             flash('You were logged in')
@@ -98,7 +118,7 @@ def login():
     return render_template('login.html', error=error)
 
 
-@app.route('/logout')
+@APP.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
@@ -108,6 +128,6 @@ def logout():
 if __name__ == '__main__':
     # create our little application :)
     if DEBUG:
-        app.run()
+        APP.run()
     else:
-        app.run(host='0.0.0.0')
+        APP.run(host='0.0.0.0')
